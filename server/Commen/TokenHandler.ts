7d12e63b3c263be35893db6475bb1b965859dee8
dark_shot <ameh46870@gmail.com>
2019-02-07 11:50:55 +0330

    awdawd

--- server/Commen/TokenHandler.ts
+++ server/Commen/TokenHandler.ts
@@ -7,9 +7,8 @@ export function generateToken(user:Document):string{
     const payload = {
         userId: user._id
     };
-    // console.log(user._id);
     
-    var token = jwt.sign(payload,process.env.secret||"deFualtSecRet");
+    var token = jwt.sign(payload,(process.env.secret||"deFualtSecRet")+user._id);
     return token;
 }
 
@@ -25,11 +24,16 @@ export function disable(req:Request,res:Response,next:any){
 
 
 export function TokenMiddelware(req:Request,res:Response,next:any){
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    //@ts-ignore
+    var token = req.body.token || req.query.token || req.headers['authorization'].split(' ').pop();
+    
   // decode token
   if (token) {
+
+      
     // verifies secret and checks exp
-    jwt.verify(token, process.env.secret||"deFualtSecRet", function(err:any, decoded:any) {
+    //@ts-ignore
+    jwt.verify(token, (process.env.secret||"deFualtSecRet")+jwt.decode(token).userId, function(err:any, decoded:any) {
         if (err) {
             return res.json({ success: false, message: 'Failed to authenticate token.' ,auth_error:true});
         } else {
